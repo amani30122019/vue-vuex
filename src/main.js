210c47b9d6f82b050b19607e0f8cc6f5213fd07a
amani30122019 <at9621439@gmail.com>
2022-07-28 14:25:34 +0300

    Adding UI libraries such as vee-validate,bootstrap and fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// fontawesome
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App).use(store).use(router).mount('#app')
